5ab728230964f7b6ebb6da4ceca7170214ab615b
wizardy0ga <90923369+wizardy0ga@users.noreply.github.com>
2025-06-03 20:44:39 -0700

    Add debug statements to original file

--- Source/Header/HashyCallsOriginal.h
+++ Source/Header/HashyCallsOriginal.h
@@ -1,22 +1,31 @@
-#pragma once
-#include <windows.h>
-#include <stdio.h>
+# pragma once
+# include <windows.h>
 
-#define GLOBAL_ARCHITECTURE
+# define GLOBAL_ARCHITECTURE
+# define hc_DEBUG
 
-#define SEED_HASH 8888
+# define SEED_HASH 8888
+
+# ifdef hc_DEBUG
+# include <stdio.h>
+# define hc_dbg(msg, ...) printf("[DEBUG]::Hashycalls.%s.L%d -> " msg "\n", __func__, __LINE__, ##__VA_ARGS__)
+# endif
+
+# ifndef hc_DEBUG
+# define hc_dbg(msg, ...) do {} while (0)
+# endif
 
 /* DLL Hashes */
-#define KERNEL32 0xA52DE12A         // kernel32.dll
-#define NTDLL 0xCE561B5C            // ntdll.dll
-#define KERNELBASE 0xF6D4FD9C       // kernelbase.dll
+# define hc_KERNEL32        0xA52DE12A         // kernel32.dll
+# define hc_NTDLL           0xCE561B5C         // ntdll.dll
+# define hc_KERNELBASE      0xF6D4FD9C         // kernelbase.dll
 
 /* Internal function hashes */
-#define LoadLibraryA_Hash 0xF4DAB6A4     // LoadLibraryA
-#define FindFirstFileA_Hash 0x4CE39F26  // FindFirstFileA
-#define FindNextFileA_Hash 0xEF6B1DC1   // FindNextFileA
-#define NtAllocateVirtualMemory_Hash 0x882B79D1
-#define AddRefActCtx_Hash 0x28CE98FF
+# define LoadLibraryA_Hash              0xF4DAB6A4    // LoadLibraryA
+# define FindFirstFileA_Hash            0x4CE39F26    // FindFirstFileA
+# define FindNextFileA_Hash             0xEF6B1DC1    // FindNextFileA
+# define NtAllocateVirtualMemory_Hash   0x882B79D1
+# define AddRefActCtx_Hash              0x28CE98FF
 
 typedef NTSTATUS(NTAPI* fpNtAllocateVirtualMemory)(
     HANDLE    ProcessHandle,
@@ -25,30 +34,32 @@ typedef NTSTATUS(NTAPI* fpNtAllocateVirtualMemory)(
     PSIZE_T   RegionSize,
     ULONG     AllocationType,
     ULONG     Protect
-);
+    );
 
 typedef void(WINAPI* fpAddRefActCtx)(
-  HANDLE hActCtx
-);
+    HANDLE hActCtx
+    );
+
+typedef int(WINAPI* fpMessageBoxA)(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
 
 /* Hashed strings */
-#define WINDIR 0xFCB19529           // windir
-#define SYSTEM32 0x8CDECC66         // system32
+# define WINDIR 0xFCB19529           // windir
+# define SYSTEM32 0x8CDECC66         // system32
 
 /* testing */
-#define SH_CHANGE_NOTIFY_REGISTER 0x7D8FCBE9
-#define SHELL32 0xE2D33CAD
+# define SH_CHANGE_NOTIFY_REGISTER 0x7D8FCBE9
+# define hc_SHELL32 0xE2D33CAD
 
-#define LOCATE_KERNEL32_FUNCTION(ApiCallName) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(KERNEL32), ApiCallName##_Hash); \
+# define LOCATE_KERNEL32_FUNCTION(ApiCallName) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(hc_KERNEL32), ApiCallName##_Hash); \
 if (!ApiCallName##_) { return FALSE; }\
 
-#define LOCATE_KERNELBASE_FUNCTION(ApiCallName) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(KERNELBASE), ApiCallName##_Hash); \
+# define LOCATE_KERNELBASE_FUNCTION(ApiCallName) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(hc_KERNELBASE), ApiCallName##_Hash); \
 if (!ApiCallName##_) { return FALSE; }\
 
-#define LOCATE_NTDLL_FUNCTION(ApiCallName) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(NTDLL), ApiCallName##_Hash); \
+# define LOCATE_NTDLL_FUNCTION(ApiCallName) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(hc_NTDLL), ApiCallName##_Hash); \
 if (!ApiCallName##_) { return FALSE; }\
 
-#define LOCATE_FUNCTION(ApiCallName, ModuleHash) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(ModuleHash), ApiCallName##_Hash); \
+# define LOCATE_FUNCTION(ApiCallName, ModuleHash) fp##ApiCallName ApiCallName##_ = (fp##ApiCallName)GetProcAddressByHash(GetModuleHandleByHash(ModuleHash), ApiCallName##_Hash); \
 if (!ApiCallName##_) { return FALSE; }\
 
 
@@ -198,15 +209,15 @@ DWORD HashString(PCHAR String) {
 */
 HMODULE GetModuleHandleByHash(IN DWORD Hash) {
 
-	CHAR ModuleNameLowerCase[MAX_PATH];
-	CHAR Letter = 0;
-	UINT Index = 0;
+    CHAR ModuleNameLowerCase[MAX_PATH];
+    CHAR Letter = 0;
+    UINT Index = 0;
     PLOADER_DATA_TABLE_ENTRY pModule = 0;
     PPROC_ENV_BLOCK pPeb = (PPROC_ENV_BLOCK)__readgsqword(0x60);
     if (!pPeb) {
         return NULL;
     }
-    
+
     for (pModule = (PLOADER_DATA_TABLE_ENTRY)pPeb->Ldr->InLoadOrderModuleList.Flink; pModule->DllBase != NULL; pModule = (PLOADER_DATA_TABLE_ENTRY)pModule->InLoadOrderLinks.Flink) {
         if (pModule->BaseDllName.Length && pModule->BaseDllName.Length < MAX_PATH) {
             for (Index = 0; Index < pModule->BaseDllName.Length; Index++) {
@@ -215,10 +226,12 @@ HMODULE GetModuleHandleByHash(IN DWORD Hash) {
             }
             ModuleNameLowerCase[Index++] = '\0';
             if (HashString(ModuleNameLowerCase) == Hash) {
+                hc_dbg("Resolved 0x%0.8X to %s", Hash, ModuleNameLowerCase);
                 return (HMODULE)(pModule->DllBase);
             }
         }
     }
+    hc_dbg("Could not resolve 0x%0.8X to a DLL in the PEB", Hash);
     return NULL;
 }
 
@@ -231,24 +244,25 @@ HMODULE GetModuleHandleByHash(IN DWORD Hash) {
     @return Returns the functions address or NULL if not found.
 */
 FARPROC GetProcAddressByHash(IN HMODULE hModule, IN DWORD Hash) {
-    
+
     /* Check NT Header */
     ULONG_PTR         Base = (ULONG_PTR)hModule;
     PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)Base;
     PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)(Base + pDos->e_lfanew);
     if (pNt->Signature != IMAGE_NT_SIGNATURE) {
+        hc_dbg("NT Siganture mismatch. Got 0x%0.4X, Expected 0x%0.4X", pNt->Signature, IMAGE_NT_SIGNATURE);
         return 0;
     }
-    
+
     /* Get function stuff from export directory */
     PIMAGE_EXPORT_DIRECTORY pExportDir = (PIMAGE_EXPORT_DIRECTORY)(Base + pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
     PDWORD   pAddresses  = (PDWORD)(Base + pExportDir->AddressOfFunctions),
              pNames      = (PDWORD)(Base + pExportDir->AddressOfNames);
     PWORD    pOrdinals   = (PWORD)(Base + pExportDir->AddressOfNameOrdinals);
-    
+
     /* Get address of .text section for forwarded function checks */
     PIMAGE_SECTION_HEADER pSection = IMAGE_FIRST_SECTION(pNt),
-                          pText = 0;
+        pText = 0;
     for (unsigned int i = 0; i < pNt->FileHeader.NumberOfSections; i++, pSection++) {
         if (pSection->Characteristics & IMAGE_SCN_MEM_READ && pSection->Characteristics & IMAGE_SCN_MEM_EXECUTE) {
             pText = pSection;
@@ -290,10 +304,13 @@ FARPROC GetProcAddressByHash(IN HMODULE hModule, IN DWORD Hash) {
 
                 FunctionAddress = (ULONG_PTR)GetProcAddressByHash(hModule, HashString((PCHAR)(FunctionAddress + Offset)));
             }
+
+            hc_dbg("Resolved 0x%0.8X to %s at 0x%p", Hash, (PCHAR)(Base + pNames[i]), (PVOID)FunctionAddress);
             return (FARPROC)FunctionAddress;
         }
     }
 
+    hc_dbg("Could not resolve 0x%0.8X to any function address", Hash);
     return 0;
 }
 
@@ -321,13 +338,13 @@ typedef struct _API_CALL_LIST {
 
 API_CALL_LIST HashedAPI = {
     .CreateRemoteThread.Hash = 0x617A28E4,
-    .CreateRemoteThread.ModuleHash = KERNEL32,
+    .CreateRemoteThread.ModuleHash = hc_KERNEL32,
     .VirtualAlloc.Hash = 0x4F80A102,
-    .VirtualAlloc.ModuleHash = KERNEL32,
+    .VirtualAlloc.ModuleHash = hc_KERNEL32,
     .VirtualProtect.Hash = 0x9F90E99C,
-    .VirtualProtect.ModuleHash = KERNEL32,
+    .VirtualProtect.ModuleHash = hc_KERNEL32,
     .SHChangeNotifyRegister.Hash = SH_CHANGE_NOTIFY_REGISTER,
-    .SHChangeNotifyRegister.ModuleHash = SHELL32,
+    .SHChangeNotifyRegister.ModuleHash = hc_SHELL32,
     .BaseRegFlushKey.ModuleHash = 0x47CF92D4,
     .BaseRegFlushKey.Hash = 0x4EAB0266
 };
@@ -351,7 +368,7 @@ SIZE_T GetEnvVarByHash( IN DWORD Hash, OUT PCHAR OutBuffer) {
         if ((StringSize = StringLengthW((LPCWSTR)pEnvironment)) != 0) {
             pTmp = pEnvironment;
             Index = 0;
-            
+
             /* Locate the name of the environment variable. Stop copying to buffer when '=' sign is reached. */
             while (*pTmp != '=') {
                 VarNameBufferW[Index] = *pTmp++;
@@ -359,29 +376,30 @@ SIZE_T GetEnvVarByHash( IN DWORD Hash, OUT PCHAR OutBuffer) {
             }
             VarNameBufferW[Index] = '\0';
             WCharToChar(VarNameBufferA, (PWCHAR)VarNameBufferW);
-           
+
             if (HashString(VarNameBufferA) == Hash) {
                 WCharToChar(OutBuffer, (PWCHAR)(pEnvironment + Index + sizeof(WCHAR)));
+                hc_dbg("Resolved 0x%0.8X to (%s). Got value (%s).", Hash, VarNameBufferA, OutBuffer);
                 return StringLengthA(OutBuffer);
             }
-            
+
         }
         else {
             break;
         }
         pEnvironment += (StringSize * sizeof(WCHAR)) + sizeof(WCHAR);
     }
-
+    hc_dbg("Could not translate 0x%0.8X to any environment variables", Hash);
     return FALSE;
 }
 
 /*
-    @brief 
+    @brief
         Loads a dll from system32 using the hash of the dll file name.
 
     @param Hash - A hash of the dll file to be loaded in the process
 
-    @return Returns a handle to the module or NULL if the dll was not loaded. 
+    @return Returns a handle to the module or NULL if the dll was not loaded.
 */
 HMODULE LoadDllFromSystem32ByHash(IN DWORD Hash) {
 
@@ -394,7 +412,7 @@ HMODULE LoadDllFromSystem32ByHash(IN DWORD Hash) {
     LOCATE_KERNEL32_FUNCTION(LoadLibraryA);
     LOCATE_KERNEL32_FUNCTION(FindFirstFileA);
     LOCATE_KERNEL32_FUNCTION(FindNextFileA);
-    
+
     /* Create the search string c:\windows\* */
     SIZE_T VarSize = GetEnvVarByHash(WINDIR, DirSearchString);
     if (VarSize == 0 || VarSize > MAX_PATH)
@@ -405,9 +423,9 @@ HMODULE LoadDllFromSystem32ByHash(IN DWORD Hash) {
     if ((hFile = FindFirstFileA_(DirSearchString, &FileData)) == INVALID_HANDLE_VALUE) {
         return NULL;
     }
-    do 
+    do
     {
-        if (HashString(FileData.cFileName) == SYSTEM32) 
+        if (HashString(FileData.cFileName) == SYSTEM32)
         {
             DirSearchString[StringLengthA(DirSearchString) - 1] = '\0'; /* C:\Windows\* -> C:\Windows */
             StringConcatA(DirSearchString, FileData.cFileName);         /* C:\Windows\System32        */
@@ -427,10 +445,12 @@ HMODULE LoadDllFromSystem32ByHash(IN DWORD Hash) {
     do {
         ToLower(FileData.cFileName);
         if (HashString(FileData.cFileName) == Hash) {
+            hc_dbg("Resolved 0x%0.8X to %s", Hash, FileData.cFileName);
             return LoadLibraryA_(FileData.cFileName);
         }
     } while (FindNextFileA_(hFile, &FileData) != 0);
 
+    hc_dbg("Could not resolve 0x%0.8X to any dll in system32", Hash);
     return NULL;
 }
 
@@ -441,17 +461,27 @@ HMODULE LoadDllFromSystem32ByHash(IN DWORD Hash) {
 */
 BOOL InitApiCalls() {
     PAPI_CALL pApiCall = (PAPI_CALL)&HashedAPI;
-    if (!(&HashedAPI)->Initialized) {
-        for (int i = 0; i < (sizeof(API_CALL_LIST) / sizeof(API_CALL)); i++) {
-            if ((pApiCall->hModule = GetModuleHandleByHash(pApiCall->ModuleHash)) == NULL) {
-                if ((pApiCall->hModule = LoadDllFromSystem32ByHash(pApiCall->ModuleHash)) == NULL) {
+    
+    hc_dbg("Beginning hashed API resolution...");
+    
+    if ( !(&HashedAPI)->Initialized ) {
+        for ( int i = 0; i < (sizeof(API_CALL_LIST) / sizeof(API_CALL)); i++ ) {
+            hc_dbg("Resolving function %i of %zu [Function: 0x%0.8X, Dll: 0x%0.8X]", (i + 1), (sizeof(API_CALL_LIST) / sizeof(API_CALL)), pApiCall->Hash, pApiCall->ModuleHash);
+
+            if ( (pApiCall->hModule = GetModuleHandleByHash(pApiCall->ModuleHash)) == NULL ) {
+                hc_dbg("No module was found. Loading module from c:\\windows\\system32...");
+                
+                if ( (pApiCall->hModule = LoadDllFromSystem32ByHash(pApiCall->ModuleHash)) == NULL ) {
+                    hc_dbg("Could not find dll file in system32 matching hash 0x%0.8X", pApiCall->ModuleHash);
                     return FALSE;
                 }
             }
-            if ((pApiCall->Address = GetProcAddressByHash(pApiCall->hModule, pApiCall->Hash)) == NULL) {
+
+            if ( (pApiCall->Address = GetProcAddressByHash(pApiCall->hModule, pApiCall->Hash) ) == NULL) {
+                hc_dbg("Could not find a function address for the hash 0x%0.8X", pApiCall->Hash);
                 return FALSE;
             }
-            (ULONG_PTR)pApiCall += sizeof(API_CALL);
+            pApiCall++;
         }
         (&HashedAPI)->Initialized = TRUE;
     }
